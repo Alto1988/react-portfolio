04c3335bad3fdbaf8c7b59365bc8d52e377fcace
Jacob Pagan <jake2415@gmail.com>
2021-11-12 11:57:39 -0600

    Finished the boiler plate. Credits to yakkomajuri for starter code.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+export const App = hot(_App);
+
+export default function _App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
